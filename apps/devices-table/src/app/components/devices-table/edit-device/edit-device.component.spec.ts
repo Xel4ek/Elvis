bcdeb8dcba74e2c0cd581530d99e9cbb4d51d022
Xel4ek <l-baal@yandex.ru>
2022-08-24 09:15:36 +0300

    add tests

--- apps/devices-table/src/app/components/devices-table/edit-device/edit-device.component.spec.ts
+++ apps/devices-table/src/app/components/devices-table/edit-device/edit-device.component.spec.ts
@@ -1,16 +1,33 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditDeviceComponent } from './edit-device.component';
+import { FormBuilder } from '@angular/forms';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 describe('EditDeviceComponent', () => {
   let component: EditDeviceComponent;
   let fixture: ComponentFixture<EditDeviceComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(() => {
+    TestBed.configureTestingModule({
       declarations: [EditDeviceComponent],
+      providers: [
+        FormBuilder,
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {
+            title: 'test1',
+            resolutions: [{ width: 1, height: 1 }],
+            type: 'Стационарная камера',
+          },
+        },
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(EditDeviceComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
